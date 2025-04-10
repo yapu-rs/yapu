c3ef5f1b5b529b488138ca013170bfee302df366
ttyS0 <ttyS0@tuta.io>
2025-04-10 10:41:26 +0800

    examples: better `discover` example

--- examples/discover.rs
+++ examples/discover.rs
@@ -1,7 +1,19 @@
+use clap::Parser;
 use yapu::{Probe, Programmer};
 
+#[derive(Parser)]
+struct Cli {
+    #[clap(default_value_t = 9_600u32)]
+    baudrate: u32,
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let probe = Probe::default();
+    let cli = Cli::parse();
+
+    let mut builder = Probe::builder();
+    builder.baudrate(cli.baudrate);
+    let probe = builder.build();
+
     println!("Please wait for probing...");
     for mut prog in Programmer::discover(&probe)? {
         let bootloader = prog.read_bootloader()?;
@@ -12,7 +24,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
              Opcodes: {}",
             prog.inner().name().unwrap_or("N/A".to_string()),
             bootloader.version_string(),
-            bootloader.opcodes()
+            bootloader
+                .opcodes()
                 .iter()
                 .map(|opcode| opcode.to_string())
                 .collect::<Vec<_>>()
